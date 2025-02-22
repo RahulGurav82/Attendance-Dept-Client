821a1f00304fde82593e2493d2ef09fb4b4c1b8c
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2025-02-22 23:47:38 +0530

    url 1

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://attendence-dept-server.onrender.com';
 
 export const endpoints = {
     auth: {
